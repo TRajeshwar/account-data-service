bd6474114b469457e91211803e5494796eef8bef
imajaynegi <ajaynegi61@gmail.com>
2020-07-10 12:41:23 +0530

    Initial code

---
+++ SOAP-SpringBootMongo-API/src/main/java/com/account/service/AccountService.java
@@ -0,0 +1,63 @@
+package com.account.service;
+
+
+import java.util.GregorianCalendar;
+import java.util.List;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.account.acc_ws.AccountInfo;
+import com.account.acc_ws.AccountRequest;
+import com.account.model.Account;
+import com.account.repository.AccountRepository;
+
+
+
+@Service
+public class AccountService {
+
+//	@Autowired
+//	AccountRepository accountRepository;
+	
+	@Autowired
+	AccountRepository accountRepository;
+
+	private static long createRandomId(){    
+		return (long)(Math.random()*100000 + 3333300000L);
+	}
+
+	public AccountInfo getAccountDetailsById(AccountRequest  acc) throws DatatypeConfigurationException {
+//		List<Account> accountList= (List<Account>) accountRepositoryImpl.findAll();
+		Account accountList=accountRepository.findByAccountNumber(acc.getAccountNumber());
+//		System.out.println(account1.toString());
+		AccountInfo accountInfo=new AccountInfo();		
+		XMLGregorianCalendar xmlDate = null;
+		
+		
+			GregorianCalendar gc = new GregorianCalendar();
+			gc.setTime(accountList.getOpen_date());	 
+			try{
+				xmlDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(gc);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			accountInfo.setId(createRandomId());
+			accountInfo.setAccountNumber(accountList.getAccountNumber());
+			accountInfo.setType(accountList.getType());
+			accountInfo.setOpenDate(xmlDate);
+			accountInfo.setCurrency(accountList.getCurrency());
+			accountInfo.setScheme(accountList.getScheme());
+			accountInfo.setCardNumber(accountList.getCard_number());
+			accountInfo.setCharges(accountList.getCharges());
+			accountInfo.setOverdraftAmout(accountList.getOverdraft_amount()); 
+			accountInfo.setOverdraftLimit(accountList.getOverdraft_limit());
+			accountInfo.setSortCode(accountList.getSort_code());
+			accountInfo.setActive(accountList.getActive());
+		
+		
+		return accountInfo;
+	}
+
+}
